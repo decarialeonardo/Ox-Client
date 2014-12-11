799f67fb3c21556db57e60aa93381800395a61f7
Leonardo De Caria <decarialeonardo@gmail.com>
2014-12-11 13:13:08 -0300

    PopUp de creacion de proyecto - wireframe

--- app/scripts/controllers/pipeline/dashboard.js
+++ app/scripts/controllers/pipeline/dashboard.js
@@ -8,6 +8,24 @@
  * Controller of the OxApp
  */
 angular.module('OxApp')
-  .controller('DashboardCtrl', ['$scope', function ($scope) {
-   
+  .controller('DashboardCtrl', ['$scope','$modal', function ($scope, $modal) {
+
+  		$scope.open = function(){
+  			var modalInstance = $modal.open({
+		      templateUrl: 'myModalContent.html',
+		      controller: ModalInstanceCtrl
+		    });
+  		}
+
+  		var ModalInstanceCtrl = function ($scope, $modalInstance) {
+
+			$scope.ok = function () {
+				$modalInstance.close($scope.selected.item);
+			};
+
+			$scope.cancel = function () {
+				$modalInstance.dismiss('cancel');
+			};
+		};
+
   }]);
